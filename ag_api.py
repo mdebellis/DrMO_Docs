a82805e14e28bd85b691641fbc36c7f17f3c379c
Michael DeBellis <mdebellissf@gmail.com>
2024-05-29 20:30:52 -0700

    Updated match
    Starting to use ag_api to standardize ontology access

--- ag_api.py
+++ ag_api.py
@@ -1,7 +1,7 @@
 from franz.openrdf.connect import ag_connect
 from franz.openrdf.vocabulary import RDF
 
-conn = ag_connect('drmo', host='localhost', port=10035, user='mdebellis', password='df1559')
+conn = ag_connect('drmo', host='localhost', port=10035, user='xxxxxxx', password='xxxxxxx')
 
 owl_named_individual = conn.createURI("http://www.w3.org/2002/07/owl#NamedIndividual")
 owl_datatype_property = conn.createURI("http://www.w3.org/2002/07/owl#DatatypeProperty")
@@ -9,6 +9,7 @@ owl_annotation_property = conn.createURI("http://www.w3.org/2002/07/owl#Annotati
 owl_object_property = conn.createURI("http://www.w3.org/2002/07/owl#ObjectProperty")
 owl_class = conn.createURI("http://www.w3.org/2002/07/owl#Class")
 rdfs_label_property = conn.createURI("http://www.w3.org/2000/01/rdf-schema#label")
+subclass_of_property = conn.createURI("http://www.w3.org/2000/01/rdf-schema#subClassOf")
 skos_pref_label_property = conn.createURI("http://www.w3.org/2004/02/skos/core#prefLabel")
 ontology_string = "http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#"
 
@@ -17,7 +18,7 @@ ontology_string = "http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/
 def make_iri_string (iri_name):
     return ontology_string + iri_name
 
-# Finds a class with the the IRI class_name
+# Finds a class with the IRI class_name
 # If no such class exists, returns None
 # Note: when we refer to "IRI name" we mean last part of the IRI after the ontology prefix
 # E.g., IRI name of "http://www.semanticweb.org/ontologies/2022/1/CfHA_Ontology/Person" is "Person"
@@ -32,8 +33,7 @@ def find_class (class_name):
 # Returns a list with all the instances of the class where the class is specified by name
 # Note wherever it says X_name it means the IRI name of X If no class with that IRI name returns None
 # If the class has no instances returns an empty list.
-def find_instances_of_class(class_name):
-    class_object = find_class(class_name)
+def find_instances_of_class(class_object):
     if class_object is None:
         return None
     class_list = []
@@ -56,6 +56,13 @@ def find_property(prop_str):
         return prop
     print(f'Error {prop} is not a property')
     return None
+
+def find_subclasses(class_object):
+    subclass_statements = conn.getStatements(None, subclass_of_property, class_object)
+    subclass_set = set()
+    for sc_statement in subclass_statements:
+        subclass_set.add(sc_statement.getSubject())
+    return subclass_set
 
 # Finds an instance from the IRI name
 def find_instance_from_iri(iri_name):
@@ -86,12 +93,10 @@ def find_object_from_label(label_string):
 # Gets the value of a single valued property using the IRI name of the instance and the IRI name of the property
 # If the property has multiple values prints a warning and returns the first one
 # If the property has no value returns None Note: if not sure whether property has multiple values, best to use get_values
-def get_value(iri_name, property_string):
-    instance = find_instance_from_iri(iri_name)
+def get_value(instance, owl_property):
     if instance is None:
         print("Error no object with iri name: {iri_name}")
         return None
-    owl_property = find_property(property_string)
     statements = conn.getStatements(instance, owl_property, None)
     with statements:
         for statement in statements:
@@ -100,16 +105,14 @@ def get_value(iri_name, property_string):
                 return statement.getObject()
             elif len(statements) == 1:
                 return statement.getObject()
-    print(f'Error: No property value for: {iri_name, property_string}.')
+    print(f'Error: No property value for: {instance, owl_property}.')
     return None
 
 # Returns the values of a the property of an instance in a list if no values returns an empty list
-def get_values (iri_name, property_string):
-    instance = find_instance_from_iri(iri_name)
+def get_values (instance, owl_property):
     if instance == None:
         print("Error no object with iri name: {iri_name}")
         return None
-    owl_property = find_property(property_string)
     values = []
     statements = conn.getStatements(instance, owl_property, None)
     with statements:
@@ -119,9 +122,8 @@ def get_values (iri_name, property_string):
     return values
 
 # Creates a new instance of a class and returns the new instance
-def make_instance (instance_name, class_name):
+def make_instance (instance_name, instance_class):
     instance_iri = conn.createURI(make_iri_string(instance_name))
-    instance_class = find_class(class_name)
     conn.add(instance_iri, RDF.TYPE, owl_named_individual)
     conn.add(instance_iri, RDF.TYPE, instance_class)
     return instance_iri
@@ -162,36 +164,38 @@ def put_value(instance, kg_property, new_value):
 def delete_value(instance, kg_property, old_value):
     conn.removeTriples(instance, kg_property, old_value)
 
-"""
-#Test data, in each case the comment below is what should be returned (with the current ontology)
-# Note: these were made for another ontology originally so these examples won't work. 
-# I'm going to update them when I get a chance for drmo
-print(find_instance_from_iri("AMerry"))
-# <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#AMerry>
-print(get_values("AMerry", "isAuthorOf"))
-# [<http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#TestDocumentAM>,...]
-print(get_value("AMerry", "lastName"))
-# "Merry"
-print(convert_to_string(get_value("AMerry", "lastName")))
-# mdebellissf@gmail.com
-print(find_class("DentalMaterialProduct"))
-# <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#DentalMaterialProduct>
-print(find_class("Foo"))
-# Error Foo is not a class
-# None
-print(find_instances_of_class("JournalArticle"))
-# [<http://www.semanticweb.org/ontologies/2022/1/CfHA_Ontology/DanielDuffy>, <http://www.semanticweb.org/ontologies/2022/1/CfHA_Ontology/RyanMcGranaghan>,...]
-print(object_to_string(find_class("JournalArticle")))
-# "Journal Article"@en
-put_value(find_instance_from_iri("AMerry"), find_property("isAuthorOf"), make_instance("TestDocumentAM", "JournalArticle"))
-print(get_values("AMerry", "isAuthorOf"))
-# [<http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#TestDocumentAM>,...]
-# List should include TestDocumentAM
-delete_value(find_instance_from_iri("AMerry"), find_property("isAuthorOf"),find_instance_from_iri("TestDocumentAM"))
-print(get_values("AMerry", "isAuthorOf"))
-# List should not include TestDocumentAM
-print(find_object_from_label("A Marie"))
-# <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#AMarie>
-# Note: always use find_object_from_iri when possible, it should be much faster
-conn.deleteDuplicates("spo")   # So we can run the test data without creating duplicates
-"""
+
+# #Test data, in each case the comment below is what should be returned (with the current ontology)
+# # Note: these were made for another ontology originally so these examples won't work.
+# # I'm going to update them when I get a chance for drmo
+# print(find_instance_from_iri("AMerry"))
+# # <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#AMerry>
+# print(get_values(find_instance_from_iri("AMerry"), find_property("isAuthorOf")))
+# # [<http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#TestDocumentAM>,...]
+# print(get_value(find_instance_from_iri("AMerry"), find_property("lastName")))
+# # "Merry"
+# print(convert_to_string(get_value(find_instance_from_iri("AMerry"), find_property("lastName"))))
+# # mdebellissf@gmail.com
+# print(find_class("DentalMaterialProduct"))
+# # <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#DentalMaterialProduct>
+# print(find_class("Foo"))
+# # Error Foo is not a class
+# # None
+# print(find_instances_of_class(find_class("JournalArticle")))
+# # [<http://www.semanticweb.org/ontologies/2022/1/CfHA_Ontology/DanielDuffy>, <http://www.semanticweb.org/ontologies/2022/1/CfHA_Ontology/RyanMcGranaghan>,...]
+# print(object_to_string(find_class("JournalArticle")))
+# # "Journal Article"@en
+# put_value(find_instance_from_iri("AMerry"), find_property("isAuthorOf"), make_instance("TestDocumentAM", "JournalArticle"))
+# print(get_values(find_instance_from_iri("AMerry"), find_property("isAuthorOf")))
+# # [<http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#TestDocumentAM>,...]
+# # List should include TestDocumentAM
+# delete_value(find_instance_from_iri("AMerry"), find_property("isAuthorOf"), find_instance_from_iri("TestDocumentAM"))
+# print(get_values(find_instance_from_iri("AMerry"), find_property("isAuthorOf")))
+# # List should not include TestDocumentAM
+# print(find_object_from_label("A Marie"))
+# # <http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#AMarie>
+# # Note: always use find_object_from_iri when possible, it should be much faster
+# print(find_subclasses(find_class("DentalMaterialProduct")))
+# print(len(find_subclasses(find_class("DentalMaterialProduct"))))
+# conn.deleteDuplicates("spo")   # So we can run the test data without creating duplicates
+
--- match.py
+++ match.py
@@ -1,54 +1,55 @@
-from typing import Dict, Any
-
-from franz.openrdf.sail.allegrographserver import AllegroGraphServer
 from franz.openrdf.connect import ag_connect
 from franz.openrdf.vocabulary import RDF
+from ag_api import *
 
-
-
-conn = ag_connect('drmo', host='localhost', port='10035',
-                  user='mdebellis', password='df1559')
 
 
 domain_ont_str = "http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#"
-keyword_property = conn.createURI('http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#keywords')
-rdf_type = conn.createURI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-owl_named_individual = conn.createURI("http://www.w3.org/2002/07/owl#NamedIndividual")
-skos_concept_class = conn.createURI("http://www.w3.org/2004/02/skos/core#Concept")
-owl_class = conn.createURI("http://www.w3.org/2002/07/owl#Class")
+keyword_property = conn.createURI(domain_ont_str + 'keywords')
+#owl_named_individual = conn.createURI("http://www.w3.org/2002/07/owl#NamedIndividual")
+#skos_concept_class = conn.createURI("http://www.w3.org/2004/02/skos/core#Concept")
+#owl_class = conn.createURI("http://www.w3.org/2002/07/owl#Class")
+#subclass_of_property = conn.createURI("http://www.w3.org/2000/01/rdf-schema#subClassOf")
 
-label_property = conn.createURI('http://www.w3.org/2000/01/rdf-schema#label')
-has_subject_prop = conn.createURI('http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#hasSubject')
-document_class = conn.createURI('http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#Document')
 
-dental_product_class = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#DentalMaterialProduct")
-is_about_product_prop = conn.createURI('http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#isAboutProduct')
+has_subject_prop = conn.createURI(domain_ont_str + 'hasSubject')
+document_class = conn.createURI(domain_ont_str + 'Document')
+section_class = conn.createURI(domain_ont_str + 'Section')
+measurement_class = conn.createURI(domain_ont_str + 'Measurement')
 
-is_about_prop = conn.createURI('http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#isAbout')
-
+dental_product_class = conn.createURI(domain_ont_str + 'DentalMaterialProduct')
+is_about_product_prop = conn.createURI(domain_ont_str + 'isAboutProduct')
+is_about_prop = conn.createURI('http://purl.obolibrary.org/obo/IAO_0000136')
+documented_in_prop = conn.createURI(domain_ont_str + 'isDocumentedIn')
 abstract_property = conn.createURI('http://purl.org/dc/terms/abstract')
 
 stop_words1 = {'those', 'on', 'own', '’ve', 'yourselves', 'around', 'between', 'four', 'been', 'alone', 'off', 'am', 'then', 'other', 'can', 'regarding', 'hereafter', 'front', 'too', 'used', 'wherein', '‘ll', 'doing', 'everything', 'up', 'onto', 'never', 'either', 'how', 'before', 'anyway', 'since', 'through', 'amount', 'now', 'he', 'was', 'have', 'into', 'because', 'not', 'therefore', 'they', 'n’t', 'even', 'whom', 'it', 'see', 'somewhere', 'thereupon', 'nothing', 'whereas', 'much', 'whenever', 'seem', 'until', 'whereby', 'at', 'also', 'some', 'last', 'than', 'get', 'already', 'our', 'once', 'will', 'noone', "'m", 'that', 'what', 'thus', 'no', 'myself', 'out', 'next', 'whatever', 'although', 'though', 'which', 'would', 'therein', 'nor', 'somehow', 'whereupon', 'besides', 'whoever', 'ourselves', 'few', 'did', 'without', 'third', 'anything', 'twelve', 'against', 'while', 'twenty', 'if', 'however', 'herself', 'when', 'may', 'ours', 'six', 'done', 'seems', 'else', 'call', 'perhaps', 'had', 'nevertheless', 'where', 'otherwise', 'still', 'within', 'its', 'for', 'together', 'elsewhere', 'throughout', 'of', 'others', 'show', '’s', 'anywhere', 'anyhow', 'as', 'are', 'the', 'hence', 'something', 'hereby', 'nowhere', 'latterly', 'say', 'does', 'neither', 'his', 'go', 'forty', 'put', 'their', 'by', 'namely', 'could', 'five', 'unless', 'itself', 'is', 'nine', 'whereafter', 'down', 'bottom', 'thereby', 'such', 'both', 'she', 'become', 'whole', 'who', 'yourself', 'every', 'thru', 'except', 'very', 'several', 'among', 'being', 'be', 'mine', 'further', 'n‘t', 'here', 'during', 'why', 'with', 'just', "'s", 'becomes', '’ll', 'about', 'a', 'using', 'seeming', "'d", "'ll", "'re", 'due', 'wherever', 'beforehand', 'fifty', 'becoming', 'might', 'amongst', 'my', 'empty', 'thence', 'thereafter', 'almost', 'least', 'someone', 'often', 'from', 'keep', 'him', 'or', '‘m', 'top', 'her', 'nobody', 'sometime', 'across', '‘s', '’re', 'hundred', 'only', 'via', 'name', 'eight', 'three', 'back', 'to', 'all', 'became', 'move', 'me', 'we', 'formerly', 'so', 'i', 'whence', 'under', 'always', 'himself', 'in', 'herein', 'more', 'after', 'themselves', 'you', 'above', 'sixty', 'them', 'your', 'made', 'indeed', 'most', 'everywhere', 'fifteen', 'but', 'must', 'along', 'beside', 'hers', 'side', 'former', 'anyone', 'full', 'has', 'yours', 'whose', 'behind', 'please', 'ten', 'seemed', 'sometimes', 'should', 'over', 'take', 'each', 'same', 'rather', 'really', 'latter', 'and', 'ca', 'hereupon', 'part', 'per', 'eleven', 'ever', '‘re', 'enough', "n't", 'again', '‘d', 'us', 'yet', 'moreover', 'mostly', 'one', 'meanwhile', 'whither', 'there', 'toward', '’m', "'ve", '’d', 'give', 'do', 'an', 'quite', 'these', 'everyone', 'towards', 'this', 'cannot', 'afterwards', 'beyond', 'make', 'were', 'whether', 'well', 'another', 'below', 'first', 'upon', 'any', 'none', 'many', 'serious', 'various', 're', 'two', 'less', '‘ve'}
-stop_words2 = {'goal','Target', 'Indicator', '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.', '11.', '12.', '13.', '14.', '15.', '16.', '17.', '[', ']'}
+stop_words2 = {'goal', 'Target', 'Indicator', '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.', '11.', '12.', '13.', '14.', '15.', '16.', '17.', '[', ']'}
 all_stop_words = stop_words1.union(stop_words2)
 
-
 conn.createFreeTextIndex("DOC-INDEX", predicates=[keyword_property, abstract_property], stopWords=all_stop_words, wordFilters=["stem.english"])
+document_instances = find_instances_of_class(document_class)
+section_instances = find_instances_of_class(section_class)
+measurement_instances = find_instances_of_class(measurement_class)
+measurement_classes = find_subclasses(measurement_class)
 
-def create_topic_links():
-    for concept_statement in conn.getStatements(None, rdf_type, skos_concept_class):
+def create_instance_about_links():
+    print("Creating instance is about links...")
+    for concept_statement in conn.getStatements(None, RDF.TYPE, owl_named_individual):
         concept = concept_statement[0]
-        concept_labels = conn.getStatements(concept, label_property, None)
-        for label_statement in concept_labels:
-            label_value = label_statement[2]
-            label = str(label_value)
-            print(label)
-            for doc_string_list in conn.evalFreeTextSearch(label):
-                # The FTI returns a list of strings, not IRIs so it is necessary to use createURI to find the actual object
-                doc_str = doc_string_list[0]
-                print(doc_str)
-                doc = conn.createURI(doc_str)
-                conn.add(doc, is_about_prop, concept)
+        if concept not in document_instances and concept not in measurement_instances and concept not in section_instances:
+            concept_labels = conn.getStatements(concept, rdfs_label_property, None)
+            print("processing instance " + str(concept))
+            for label_statement in concept_labels:
+                label_value = label_statement[2]
+                label = str(label_value)
+                print(label)
+                for doc_string_list in conn.evalFreeTextSearch(label):
+                    doc_str = doc_string_list[0]
+                    print(doc_str)
+                    doc = conn.createURI(doc_str)
+                    conn.add(doc, is_about_prop, concept)
+                    conn.add(concept, documented_in_prop, doc)
 
 
 def create_keyword_list(kw_string):
@@ -59,22 +60,44 @@ def create_keyword_list(kw_string):
     return result_list
 
 def create_products_links():
-    for product_statement in conn.getStatements(None, rdf_type, dental_product_class):
+    print("Creating products links...")
+    for product_statement in conn.getStatements(None, RDF.TYPE, dental_product_class):
         product = product_statement[0]
-        product_labels = conn.getStatements(product, label_property, None)
+        product_labels = conn.getStatements(product, rdfs_label_property, None)
         for label_statement in product_labels:
             label_value = label_statement[2]
             label = str(label_value)
             print(label)
             for doc_string_list in conn.evalFreeTextSearch(label):
-                # The FTI returns a list of strings, not IRIs so it is necessary to use createURI to find the actual object
+                # The FTI returns a list of strings, not IRIs, so it is necessary to use createURI to find the actual object
                 doc_str = doc_string_list[0]
                 print(doc_str)
                 doc = conn.createURI(doc_str)
                 conn.add(doc, is_about_product_prop, product)
+                conn.add(product, documented_in_prop, doc)
+
+def create_class_about_links():
+    print("Creating class is about links...")
+    for class_statement in conn.getStatements(None, RDF.TYPE, owl_class):
+        an_owl_class = class_statement.getSubject()
+        if an_owl_class not in measurement_classes:
+            class_labels = conn.getStatements(an_owl_class, rdfs_label_property, None)
+            for label_statement in class_labels:
+                label_value = label_statement[2]
+                label = str(label_value)
+                print(label)
+                for doc_string_list in conn.evalFreeTextSearch(label):
+                    doc_str = doc_string_list[0]
+                    print(doc_str)
+                    doc = conn.createURI(doc_str)
+                    conn.add(doc, is_about_prop, an_owl_class)
+                    conn.add(an_owl_class, documented_in_prop, doc)
 
 
 #create_products_links()
+#create_class_about_links()
+create_instance_about_links()
+
 
 
 
